f128187e609a0e552e6ad7a1849811aa9728e68f
Laurent Ellerbach <laurelle@microsoft.com>
2021-11-17 15:30:07 +0100

    Add bin export option (#5)

--- nanoFrameworkDeployer/Program.cs
+++ nanoFrameworkDeployer/Program.cs
@@ -83,6 +83,20 @@ namespace nanoFrameworkFlasher
                     _returnvalue = 1;
                     return;
                 }
+
+                if(_options.BinaryFileOnly)
+                {
+                    List<byte[]> assFiles = CreateBinDeploymentFile(peFiles);
+                    FileStream deploymentFile = File.Create(Path.Combine(workingDirectory, "deploy.bin"));
+                    foreach (var assFile in assFiles)
+                    {
+                        deploymentFile.Write(assFile.ToArray(), 0, assFile.Length);
+                    }
+
+                    deploymentFile.Close();
+                    deploymentFile.Dispose();
+                    return;
+                }
             }
             else
             {
@@ -111,7 +125,6 @@ namespace nanoFrameworkFlasher
 
             _message.Verbose("Finding valid ports");
 
-            List<byte[]> assemblies = new List<byte[]>();
             int retryCount = 0;            
             // Only 3 tries for a specified port
             numberOfRetries = string.IsNullOrEmpty(_options.ComPort) ? 10 : 3;
@@ -212,28 +225,7 @@ namespace nanoFrameworkFlasher
 
             _message.Verbose($"Added {peFiles.Length} assemblies to deploy.");
 
-            // Keep track of total assembly size
-            long totalSizeOfAssemblies = 0;
-
-            // now we will deploy all system assemblies
-            foreach (string peItem in peFiles)
-            {
-                // append to the deploy blob the assembly
-                using (FileStream fs = File.Open(peItem, FileMode.Open, FileAccess.Read))
-                {
-                    long length = (fs.Length + 3) / 4 * 4;
-                    _message.Verbose($"Adding {peItem} v0 ({length} bytes) to deployment bundle");
-                    byte[] buffer = new byte[length];
-
-                    fs.Read(buffer, 0, (int)fs.Length);
-                    assemblies.Add(buffer);
-
-                    // Increment totalizer
-                    totalSizeOfAssemblies += length;
-                }
-            }
-
-            _message.Output($"Deploying {peFiles.Length:N0} assemblies to device... Total size in bytes is {totalSizeOfAssemblies}.");
+            List<byte[]> assemblies = CreateBinDeploymentFile(peFiles);
 
             // need to keep a copy of the deployment blob for the second attempt (if needed)
             var assemblyCopy = new List<byte[]>(assemblies);
@@ -255,6 +247,34 @@ namespace nanoFrameworkFlasher
             {
                 _message.Output("Write successful");
             }
+        }
+
+        private static List<byte[]> CreateBinDeploymentFile(string[] peFiles)
+        {
+            // Keep track of total assembly size
+            long totalSizeOfAssemblies = 0;
+            List<byte[]> assemblies = new List<byte[]>();
+            // now we will deploy all system assemblies
+            foreach (string peItem in peFiles)
+            {
+                // append to the deploy blob the assembly
+                using (FileStream fs = File.Open(peItem, FileMode.Open, FileAccess.Read))
+                {
+                    long length = (fs.Length + 3) / 4 * 4;
+                    _message.Verbose($"Adding {peItem} v0 ({length} bytes) to deployment bundle");
+                    byte[] buffer = new byte[length];
+
+                    fs.Read(buffer, 0, (int)fs.Length);
+                    assemblies.Add(buffer);
+
+                    // Increment totalizer
+                    totalSizeOfAssemblies += length;
+                }
+            }
+
+            _message.Output($"Deploying {peFiles.Length:N0} assemblies to device... Total size in bytes is {totalSizeOfAssemblies}.");
+
+            return assemblies;
         }
     }
 }
