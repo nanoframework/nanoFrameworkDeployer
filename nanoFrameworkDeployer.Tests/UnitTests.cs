b970a47769cb53ae13caa7626876595cb6cba929
Robin Jones <networkfusion@users.noreply.github.com>
2022-11-21 16:51:58 +0000

    Add unit test skeleton

---
+++ nanoFrameworkDeployer.Tests/UnitTests.cs
@@ -0,0 +1,31 @@
+namespace nanoFrameworkDeployer.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void CheckCommandLineOptions()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void CheckDirectoryContainsPeFiles()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void CheckPortExclusionFileWorks()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void CheckCreateDeploymentFile()
+        {
+            Assert.IsTrue(false);
+        }
+
+    }
+}
