ca51eadbe3cd01be0dcecc67b1721a3954f6b83a
Robin Jones <networkfusion@users.noreply.github.com>
2022-11-24 02:48:39 +0000

    Add working tests for port exclusion
    Some fail
    Some pass but show issues!

--- nanoFrameworkDeployer.Tests/PortExclusionTests.cs
+++ nanoFrameworkDeployer.Tests/PortExclusionTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace nanoFrameworkDeployer.Tests
@@ -9,30 +10,7 @@ namespace nanoFrameworkDeployer.Tests
     public class PortExclusionTests
     {
         [TestMethod]
-        public void CheckPortExclusionWhenFileExistsAndIsValid()
-        {
-            // False set until test is written
-            Assert.IsTrue(true);
-        }
-
-        [TestMethod]
-        public void CheckPortExclusionWhenFileExistsAndIsInvalid()
-        {
-            // False set until test is written
-            Assert.IsTrue(true);
-        }
-
-        [TestMethod]
-        public void CheckPortExclusionWhenFileDoesNotExist()
-        {
-            // False set until test is written
-            Assert.IsTrue(true);
-        }
-
-
-        //https://github.com/reductech/TesseractConnector/blob/60203bc75b1b4f194ff6295d2ecaca6e287deeb4/Tesseract.Tests/TesseractOCRTests.cs
-        [TestMethod]
-        public void Program_Validate_ExcludingPortFileIsAwsome()
+        public void CheckPortExclusionWhenFileExistsAndIsValidSinglePort()
         {
             // Arrange
             var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
@@ -43,19 +21,95 @@ namespace nanoFrameworkDeployer.Tests
             List<string> excludedPorts = null;
             var exclusionFilePath = "c:\\portExclusions.txt";
 
-            try
-            {
-                // Act
-                Program.AddSerialPortExclusions(ref excludedPorts, exclusionFilePath);
-            }
-            catch (NotSupportedException ex)
-            {
-                // Assert
-                Assert.AreEqual("We can't go on together. It's not me, it's you.", ex.Message);
-                return;
-            }
+            // Act
+            Program.AddSerialPortExclusions(ref excludedPorts, exclusionFilePath);
 
-            Assert.Fail("The expected exception was not thrown.");
+
+            // Assert
+            Assert.AreEqual("Testing is meh.", excludedPorts.First());
+        }
+
+        [TestMethod]
+        public void CheckPortExclusionWhenFileExistsAndIsValidMultiplePorts()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+             {
+                 { @"c:\portExclusions.txt", new MockFileData("Testing is meh."+ Environment.NewLine + "line 2") },
+             });
+            var component = new Program(fileSystem);
+            List<string> excludedPorts = null;
+            var exclusionFilePath = "c:\\portExclusions.txt";
+
+            // Act
+            Program.AddSerialPortExclusions(ref excludedPorts, exclusionFilePath);
+
+            var portlist = string.Join(Environment.NewLine, excludedPorts);
+            // Assert
+            // need to be a sting array to pass!
+            Assert.AreEqual("Testing is meh." + Environment.NewLine + "line 2", portlist);
+        }
+
+        [TestMethod]
+        public void CheckPortExclusionWhenFileExistsAndIsInvalid()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+             {
+                 { @"c:\portExclusions.txt", new MockFileData("Testing is meh.") },
+             });
+            var component = new Program(fileSystem);
+            List<string> excludedPorts = null;
+            var exclusionFilePath = "c:\\portExclusions.txt";
+
+            // Act
+            Program.AddSerialPortExclusions(ref excludedPorts, exclusionFilePath);
+
+
+            // Assert
+            Assert.AreEqual("Testing is meh.", excludedPorts.First());
+        }
+
+        [TestMethod]
+        public void CheckPortExclusionWhenFileExistsAndIsEmpty()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+             {
+                 { @"c:\portExclusions.txt", new MockFileData("") },
+             });
+            var component = new Program(fileSystem);
+            List<string> excludedPorts = null;
+            var exclusionFilePath = "c:\\portExclusions.txt";
+
+            // Act
+            Program.AddSerialPortExclusions(ref excludedPorts, exclusionFilePath);
+
+
+            //TODO: The program does not try and catch this, so we also dont...  value WILL BE NULL since we never set it!
+            // Assert
+            Assert.AreEqual("", excludedPorts.First());
+        }
+
+        [TestMethod]
+        public void CheckPortExclusionWhenFileDoesNotExist()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+             {
+                 { @"c:\portExclusions.txt", new MockFileData("") },
+             });
+            var component = new Program(fileSystem);
+            List<string> excludedPorts = null;
+            var exclusionFilePath = "c:\\portExclusions1.txt"; // invalid on purpose!
+
+            //TODO: The program does not try and catch this, so we also dont... So it WILL FAIL.
+            // Act
+            Program.AddSerialPortExclusions(ref excludedPorts, exclusionFilePath);
+
+
+            // Assert
+            Assert.AreEqual("", excludedPorts.First());
         }
 
     }
